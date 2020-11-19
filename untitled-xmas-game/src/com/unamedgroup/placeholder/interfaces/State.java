3a86f52f348eb2174a41d152b5f06621b33e968b
LeinadinBR <danielnma2001@gmail.com>
2020-11-18 21:41:31 -0300

    Só corrigindo erros de caracteres de nathan

--- untitled-xmas-game/src/com/unamedgroup/placeholder/interfaces/State.java
+++ untitled-xmas-game/src/com/unamedgroup/placeholder/interfaces/State.java
@@ -4,7 +4,7 @@ import java.awt.Graphics;
 
 
 /**
- * Interface com os metodos necess·rios para implementar todo um state
+ * Interface com os metodos necess√°rios para implementar todo um state
  * @author Nathan
  */
 public interface State {
