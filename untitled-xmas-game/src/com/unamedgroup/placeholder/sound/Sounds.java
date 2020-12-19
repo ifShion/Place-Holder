648083baae0b31a7717a71834a46e75542641dc2
TanTan <nathan16nsb@gmail.com>
2020-12-19 15:37:30 -0300

    Consertei o problema do audio em diferentes IDEs

--- untitled-xmas-game/src/com/unamedgroup/placeholder/sound/Sounds.java
+++ untitled-xmas-game/src/com/unamedgroup/placeholder/sound/Sounds.java
@@ -1,5 +1,6 @@
 package com.unamedgroup.placeholder.sound;
 
+import java.io.File;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -21,84 +22,51 @@ public class Sounds {
         
 
         public Sounds() throws UnsupportedAudioFileException {
-
+                String diretorio;
                 soundMap = new LinkedHashMap<>();
-
                 SoundInput soundinput = new SoundInput();
+
+                if (!new File("untitled-xmas-game/res").exists()) { 
+                        System.out.println("Path para: Eclipse"); 
+                        diretorio = "";
+                } else { 
+                        System.out.println("Path para: Vs Code");
+                        diretorio = "untitled-xmas-game/";
+                }
                 
                 try{
-                    try{                       
-                    	soundMap.put("close_door", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/close_door_1.wav"));
-                        soundMap.put("Door_locked", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Door_locked.wav"));
-                        soundMap.put("Door_locked_2", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Door_locked2.wav"));
-                        soundMap.put("Explosion", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Explosion_2.wav"));
-                    	soundMap.put("Cannon", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Cannon_1.wav"));
-                    	soundMap.put("close_door", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/close_door_1.wav"));
-                        soundMap.put("Exp1", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoExplosion1.wav"));
-                        soundMap.put("Exp2", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoExplosion2.wav"));
-                        soundMap.put("Exp3", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoExplosion3.wav"));
-                        soundMap.put("ExpFall", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoExplosionQueda4.wav"));
-                        soundMap.put("Fall", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/fall_sound.wav"));
-                        soundMap.put("Hit", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoHit_Hurt.wav"));
-                        soundMap.put("Hit_2", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Hit_1.wav"));
-                        soundMap.put("Hit_3", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Hit_backsound.wav"));
-                        soundMap.put("Hit_4", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Hit_Hurt.wav"));
-                        soundMap.put("HitFall1", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoHit_Queda1.wav"));
-                        soundMap.put("HitFall2", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoHit_HurtQueda2.wav"));
-                        soundMap.put("Hit_Miss", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Hit_Miss.wav"));
-                        soundMap.put("Jump1", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoJump1.wav"));
-                        soundMap.put("Jump2", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoJump2.wav"));
-                        soundMap.put("Jump3", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoJump3.wav"));
-                        soundMap.put("Loop", soundinput.getAudio("untitled-xmas-game/res/audio/Loop.wav"));
-                        soundMap.put("Menu_Navigate", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Menu_Navigate.wav"));
-                        soundMap.put("Music", soundinput.getAudio("untitled-xmas-game/res/audio/Music_cut.wav"));
-                        soundMap.put("Music_alpha", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Music_alpha.wav"));
-                        soundMap.put("pCoin1", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoPick_upCoin1.wav"));
-                        soundMap.put("pCoin2", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoPick_upCoin2.wav"));
-                        soundMap.put("pCoin3", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoPickup_Coin3.wav"));
-                        soundMap.put("pCoin4", soundinput.getAudio("untitled-xmas-game/res/audio/EfeitoPickup_Coin4.wav"));
-                        soundMap.put("Pick_up_Stick", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Pick_Stick_1.wav"));
-                        soundMap.put("Pick_up_Stick2", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Pick_Up_Stick_2.wav"));
-                        soundMap.put("Pick_up_Stick3", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Pick_Up_Stick_3.wav"));
-                        soundMap.put("Select", soundinput.getAudio("untitled-xmas-game/res/audio/Audios_0.2/Select.wav"));
-                
-                
-                    }
-                    catch (Exception e){
-                    	soundMap.put("Cannon", soundinput.getAudio("res/audio/Audios_0.2/Cannon_1.wav"));
-                        soundMap.put("Door_locked", soundinput.getAudio("res/audio/Audios_0.2/Door_locked.wav"));
-                        soundMap.put("Door_locked_2", soundinput.getAudio("res/audio/Audios_0.2/Door_locked2.wav"));
-                        soundMap.put("close_door", soundinput.getAudio("res/audio/Audios_0.2/close_door_1.wav"));
-                        soundMap.put("Exp1", soundinput.getAudio("res/audio/EfeitoExplosion1.wav"));
-                        soundMap.put("Exp2", soundinput.getAudio("res/audio/EfeitoExplosion2.wav"));
-                        soundMap.put("Exp3", soundinput.getAudio("res/audio/EfeitoExplosion3.wav"));
-                        soundMap.put("ExpFall", soundinput.getAudio("res/audio/EfeitoExplosionQueda4.wav"));
-                        soundMap.put("Explosion", soundinput.getAudio("res/audio/Audios_0.2/Explosion_2.wav"));
-                        soundMap.put("Fall", soundinput.getAudio("res/audio/Audios_0.2/fall_sound.wav"));
-                        soundMap.put("Hit", soundinput.getAudio("res/audio/EfeitoHit_Hurt.wav"));
-                        soundMap.put("Hit_2", soundinput.getAudio("res/audio/Audios_0.2/Hit_1.wav"));
-                        soundMap.put("Hit_3", soundinput.getAudio("res/audio/Audios_0.2/Hit_backsound.wav"));
-                        soundMap.put("Hit_4", soundinput.getAudio("res/audio/Audios_0.2/Hit_Hurt.wav"));
-                        soundMap.put("HitFall2", soundinput.getAudio("res/audio/EfeitoHit_HurtQueda2.wav"));
-                        soundMap.put("HitFall1", soundinput.getAudio("res/audio/EfeitoHit_Queda1.wav"));
-                        soundMap.put("Hit_Miss", soundinput.getAudio("res/audio/Audios_0.2/Hit_Miss.wav"));
-                        soundMap.put("Jump1", soundinput.getAudio("res/audio/EfeitoJump1.wav"));
-                        soundMap.put("Jump2", soundinput.getAudio("res/audio/EfeitoJump2.wav"));
-                        soundMap.put("Jump3", soundinput.getAudio("res/audio/EfeitoJump3.wav"));
-                        soundMap.put("Loop", soundinput.getAudio("res/audio/Loop.wav"));
-                        soundMap.put("Menu_Navigate", soundinput.getAudio("res/audio/Audios_0.2/Menu_Navigate.wav"));
-                        soundMap.put("Music",soundinput.getAudio("res/audio/Music_cut.wav"));
-                        soundMap.put("Music_alpha", soundinput.getAudio("res/audio/Audios_0.2/Music_alpha.wav"));
-                        soundMap.put("pCoin1", soundinput.getAudio("res/audio/EfeitoPick_upCoin1.wav"));
-                        soundMap.put("pCoin2", soundinput.getAudio("res/audio/EfeitoPick_upCoin2.wav"));
-                        soundMap.put("pCoin3", soundinput.getAudio("res/audio/EfeitoPickup_Coin3.wav"));
-                        soundMap.put("pCoin4", soundinput.getAudio("res/audio/EfeitoPickup_Coin4.wav"));
-                        soundMap.put("Pick_up_Stick", soundinput.getAudio("res/audio/Audios_0.2/Pick_Stick_1.wav"));
-                        soundMap.put("Pick_up_Stick2", soundinput.getAudio("res/audio/Audios_0.2/Pick_Up_Stick_2.wav"));
-                        soundMap.put("Pick_up_Stick3", soundinput.getAudio("res/audio/Audios_0.2/Pick_Up_Stick_3.wav"));
-                        soundMap.put("Select", soundinput.getAudio("res/audio/Audios_0.2/Select.wav"));
-
-                    }
+                        soundMap.put("Cannon", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Cannon_1.wav"));
+                        soundMap.put("Door_locked", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Door_locked.wav"));
+                        soundMap.put("Door_locked_2", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Door_locked2.wav"));
+                        soundMap.put("close_door", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/close_door_1.wav"));
+                        soundMap.put("Exp1", soundinput.getAudio(diretorio+"res/audio/EfeitoExplosion1.wav"));
+                        soundMap.put("Exp2", soundinput.getAudio(diretorio+"res/audio/EfeitoExplosion2.wav"));
+                        soundMap.put("Exp3", soundinput.getAudio(diretorio+"res/audio/EfeitoExplosion3.wav"));
+                        soundMap.put("ExpFall", soundinput.getAudio(diretorio+"res/audio/EfeitoExplosionQueda4.wav"));
+                        soundMap.put("Explosion", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Explosion_2.wav"));
+                        soundMap.put("Fall", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/fall_sound.wav"));
+                        soundMap.put("Hit", soundinput.getAudio(diretorio+"res/audio/EfeitoHit_Hurt.wav"));
+                        soundMap.put("Hit_2", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Hit_1.wav"));
+                        soundMap.put("Hit_3", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Hit_backsound.wav"));
+                        soundMap.put("Hit_4", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Hit_Hurt.wav"));
+                        soundMap.put("HitFall2", soundinput.getAudio(diretorio+"res/audio/EfeitoHit_HurtQueda2.wav"));
+                        soundMap.put("HitFall1", soundinput.getAudio(diretorio+"res/audio/EfeitoHit_Queda1.wav"));
+                        soundMap.put("Hit_Miss", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Hit_Miss.wav"));
+                        soundMap.put("Jump1", soundinput.getAudio(diretorio+"res/audio/EfeitoJump1.wav"));
+                        soundMap.put("Jump2", soundinput.getAudio(diretorio+"res/audio/EfeitoJump2.wav"));
+                        soundMap.put("Jump3", soundinput.getAudio(diretorio+"res/audio/EfeitoJump3.wav"));
+                        soundMap.put("Loop", soundinput.getAudio(diretorio+"res/audio/Loop.wav"));
+                        soundMap.put("Menu_Navigate", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Menu_Navigate.wav"));
+                        soundMap.put("Music",soundinput.getAudio(diretorio+"res/audio/Music_cut.wav"));
+                        soundMap.put("Music_alpha", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Music_alpha.wav"));
+                        soundMap.put("pCoin1", soundinput.getAudio(diretorio+"res/audio/EfeitoPick_upCoin1.wav"));
+                        soundMap.put("pCoin2", soundinput.getAudio(diretorio+"res/audio/EfeitoPick_upCoin2.wav"));
+                        soundMap.put("pCoin3", soundinput.getAudio(diretorio+"res/audio/EfeitoPickup_Coin3.wav"));
+                        soundMap.put("pCoin4", soundinput.getAudio(diretorio+"res/audio/EfeitoPickup_Coin4.wav"));
+                        soundMap.put("Pick_up_Stick", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Pick_Stick_1.wav"));
+                        soundMap.put("Pick_up_Stick2", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Pick_Up_Stick_2.wav"));
+                        soundMap.put("Pick_up_Stick3", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Pick_Up_Stick_3.wav"));
+                        soundMap.put("Select", soundinput.getAudio(diretorio+"res/audio/Audios_0.2/Select.wav"));    
                 }
                 catch (Exception e) {
                         //TODO: handle exception
